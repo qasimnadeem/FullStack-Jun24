1c1bbdc9d3f4a9ad291674a8a4321c0af564fb78
Qasim Nadeem <qqasimnadeem@gmail.com>
2024-07-30 18:25:33 +0500

    new

---
+++ js-data.js
@@ -0,0 +1,18 @@
+let persons= [
+    {
+        id:1,
+        name:"amir",
+        social: ["https://facebook.com/icttrainings","https://instagram.com"]
+    },
+    {
+        id:2,
+        name:"umer",
+        social: ["https://facebook.com/icttrainings","https://instagram.com"]
+    },
+    {
+        id:3,
+        name:"ahsan",
+        social: ["https://facebook.com/icttrainings","https://instagram.com"]
+    }
+
+]
